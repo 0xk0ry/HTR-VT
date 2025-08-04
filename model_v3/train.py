6b2ab08209989e6904b93504c406045fabd996bc
kory <trucpham19092004@gmail.com>
2025-08-04 08:54:15 +0700

    Refactor train.py for improved readability; adjust code formatting and add image overlay utility for logging predictions during validation

--- model_v3/train.py
+++ model_v3/train.py
@@ -16,7 +16,8 @@ from functools import partial
 
 
 def compute_loss(args, model, image, batch_size, criterion, text, length):
-    preds = model(image, args.mask_ratio, args.max_span_length, use_masking=True)
+    preds = model(image, args.mask_ratio,
+                  args.max_span_length, use_masking=True)
     preds = preds.float()
     preds_size = torch.IntTensor([preds.size(1)] * batch_size).cuda()
     preds = preds.permute(1, 0, 2).log_softmax(2)
@@ -40,9 +41,11 @@ def main():
     writer = SummaryWriter(args.save_dir)
 
     # Initialize wandb
-    wandb.init(project="HTR-VT", name=args.exp_name, config=vars(args), dir=args.save_dir)
+    wandb.init(project="HTR-VT", name=args.exp_name,
+               config=vars(args), dir=args.save_dir)
 
-    model = HTR_VT.create_model(nb_cls=args.nb_cls, img_size=args.img_size[::-1])
+    model = HTR_VT.create_model(
+        nb_cls=args.nb_cls, img_size=args.img_size[::-1])
 
     total_param = sum(p.numel() for p in model.parameters())
     logger.info('total_param is {}'.format(total_param))
@@ -53,7 +56,8 @@ def main():
     model.zero_grad()
 
     logger.info('Loading train loader...')
-    train_dataset = dataset.myLoadDS(args.train_data_list, args.data_path, args.img_size)
+    train_dataset = dataset.myLoadDS(
+        args.train_data_list, args.data_path, args.img_size)
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=args.train_bs,
                                                shuffle=True,
@@ -63,36 +67,55 @@ def main():
     train_iter = dataset.cycle_data(train_loader)
 
     logger.info('Loading val loader...')
-    val_dataset = dataset.myLoadDS(args.val_data_list, args.data_path, args.img_size, ralph=train_dataset.ralph)
+    val_dataset = dataset.myLoadDS(
+        args.val_data_list, args.data_path, args.img_size, ralph=train_dataset.ralph)
     val_loader = torch.utils.data.DataLoader(val_dataset,
                                              batch_size=args.val_bs,
                                              shuffle=False,
                                              pin_memory=True,
                                              num_workers=args.num_workers)
 
-
     logger.info('Initializing optimizer, criterion and converter...')
-    optimizer = sam.SAM(model.parameters(), torch.optim.AdamW, lr=1e-7, betas=(0.9, 0.99), weight_decay=args.weight_decay)
+    optimizer = sam.SAM(model.parameters(), torch.optim.AdamW,
+                        lr=1e-7, betas=(0.9, 0.99), weight_decay=args.weight_decay)
     criterion = torch.nn.CTCLoss(reduction='none', zero_infinity=True)
     converter = utils.CTCLabelConverter(train_dataset.ralph.values())
 
     best_cer, best_wer = 1e+6, 1e+6
     train_loss = 0.0
 
+    # --- Helper for overlaying text on image ---
+    import torchvision.transforms as T
+    from PIL import Image, ImageDraw, ImageFont
+    def overlay_text_on_image(img_tensor, pred_text, true_text, is_correct):
+        img = T.ToPILImage()(img_tensor)
+        draw = ImageDraw.Draw(img)
+        try:
+            font = ImageFont.truetype("arial.ttf", 16)
+        except:
+            font = ImageFont.load_default()
+        color = "green" if is_correct else "red"
+        draw.text((5, 5), f"Pred: {pred_text}", fill=color, font=font)
+        draw.text((5, 25), f"GT: {true_text}", fill="blue", font=font)
+        return img
+
     #### ---- train & eval ---- ####
     logger.info('Start training...')
     for nb_iter in range(1, args.total_iter):
-        optimizer, current_lr = utils.update_lr_cos(nb_iter, args.warm_up_iter, args.total_iter, args.max_lr, optimizer)
+        optimizer, current_lr = utils.update_lr_cos(
+            nb_iter, args.warm_up_iter, args.total_iter, args.max_lr, optimizer)
 
         optimizer.zero_grad()
         batch = next(train_iter)
         image = batch[0].cuda()
         text, length = converter.encode(batch[1])
         batch_size = image.size(0)
-        loss = compute_loss(args, model, image, batch_size, criterion, text, length)
+        loss = compute_loss(args, model, image, batch_size,
+                            criterion, text, length)
         loss.backward()
         optimizer.first_step(zero_grad=True)
-        compute_loss(args, model, image, batch_size, criterion, text, length).backward()
+        compute_loss(args, model, image, batch_size,
+                     criterion, text, length).backward()
         optimizer.second_step(zero_grad=True)
         model.zero_grad()
         model_ema.update(model, num_updates=nb_iter / 2)
@@ -101,12 +124,14 @@ def main():
         if nb_iter % args.print_iter == 0:
             train_loss_avg = train_loss / args.print_iter
 
-            logger.info(f'Iter : {nb_iter} \t LR : {current_lr:0.5f} \t training loss : {train_loss_avg:0.5f} \t ' )
+            logger.info(
+                f'Iter : {nb_iter} \t LR : {current_lr:0.5f} \t training loss : {train_loss_avg:0.5f} \t ')
 
             writer.add_scalar('./Train/lr', current_lr, nb_iter)
             writer.add_scalar('./Train/train_loss', train_loss_avg, nb_iter)
             # wandb log
-            wandb.log({"train/lr": current_lr, "train/loss": train_loss_avg, "iter": nb_iter})
+            wandb.log({"train/lr": current_lr,
+                      "train/loss": train_loss_avg, "iter": nb_iter})
             train_loss = 0.0
 
         if nb_iter % args.eval_iter == 0:
@@ -118,24 +143,28 @@ def main():
                                                                              converter)
 
                 if val_cer < best_cer:
-                    logger.info(f'CER improved from {best_cer:.4f} to {val_cer:.4f}!!!')
+                    logger.info(
+                        f'CER improved from {best_cer:.4f} to {val_cer:.4f}!!!')
                     best_cer = val_cer
                     checkpoint = {
                         'model': model.state_dict(),
                         'state_dict_ema': model_ema.ema.state_dict(),
                         'optimizer': optimizer.state_dict(),
                     }
-                    torch.save(checkpoint, os.path.join(args.save_dir, 'best_CER.pth'))
+                    torch.save(checkpoint, os.path.join(
+                        args.save_dir, 'best_CER.pth'))
 
                 if val_wer < best_wer:
-                    logger.info(f'WER improved from {best_wer:.4f} to {val_wer:.4f}!!!')
+                    logger.info(
+                        f'WER improved from {best_wer:.4f} to {val_wer:.4f}!!!')
                     best_wer = val_wer
                     checkpoint = {
                         'model': model.state_dict(),
                         'state_dict_ema': model_ema.ema.state_dict(),
                         'optimizer': optimizer.state_dict(),
                     }
-                    torch.save(checkpoint, os.path.join(args.save_dir, 'best_WER.pth'))
+                    torch.save(checkpoint, os.path.join(
+                        args.save_dir, 'best_WER.pth'))
 
                 logger.info(
                     f'Val. loss : {val_loss:0.3f} \t CER : {val_cer:0.4f} \t WER : {val_wer:0.4f} \t ')
@@ -146,12 +175,24 @@ def main():
                 writer.add_scalar('./VAL/bestWER', best_wer, nb_iter)
                 writer.add_scalar('./VAL/val_loss', val_loss, nb_iter)
                 # wandb log
+                example_count = min(5, len(preds))  # log up to 5 examples
+                example_images = []
+                for i in range(example_count):
+                    img_tensor = batch[0][i].cpu()
+                    pred_text = converter.decode([preds[i]])
+                    true_text = converter.decode([labels[i]])
+                    is_correct = pred_text == true_text
+                    img_with_text = overlay_text_on_image(img_tensor, pred_text, true_text, is_correct)
+                    caption = f"Pred: {pred_text} | GT: {true_text} | {'✅' if is_correct else '❌'}"
+                    example_images.append(wandb.Image(img_with_text, caption=caption))
+
                 wandb.log({
                     "val/loss": val_loss,
                     "val/CER": val_cer,
                     "val/WER": val_wer,
                     "val/best_CER": best_cer,
                     "val/best_WER": best_wer,
+                    "val/examples": example_images,
                     "iter": nb_iter
                 })
                 model.train()
